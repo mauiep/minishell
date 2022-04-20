0e7841cbbd85fb7f9f1aef80dc01f33ab1c4f64a
Niki NIKOLOV <ngenadie@e2r1p8.clusters.42paris.fr>
2022-04-20 17:07:56 +0200

    multipipe

--- srcs/ft_exec.c
+++ srcs/ft_exec.c
@@ -54,8 +54,8 @@ char	*ft_find_bin(char *bin, char *paths, char **argv, char **envp)
 			return (NULL);
 		if (access(bin_path, F_OK & X_OK) == 0)
 		{
-			dprintf(2, "BEFORE EXEC:\n      bin_path = %s, argv[0] = %s, envp[0] = %s\n",
-					bin_path, argv[0], envp[0]);
+			dprintf(2, "BEFORE EXEC:\n      bin_path = %s, argv[0] = %s, argv[1] = %s, envp[0] = %s\n",
+					bin_path, argv[0], argv[1], envp[0]);
 			execve(bin_path, argv, envp);
 			return (bin_path);
 		}
@@ -74,7 +74,7 @@ char	*ft_pipes(int ac, char **argv, t_dynarray *darr)
 	int			pipefd[2];
 	int			i;
 	int			fd_in;
-	pid_t		list[2];
+	pid_t		list[3];
 	pid_t		main_pid;
 	char		*nul;
 
@@ -86,36 +86,33 @@ char	*ft_pipes(int ac, char **argv, t_dynarray *darr)
 	fd_in = dup(STDIN_FILENO);
 	argv[2] = NULL;
 	argv[5] = NULL;
+	argv[8] = NULL;
 	i = 0;
-	while (i < 2)
+	while (i < ac / 3)
 	{
 		pipe(pipefd);
 		main_pid = fork();
-		printf("main_pid = %d\n", main_pid);
 		list[i] = main_pid;
-		printf("after for = %d\n", main_pid);
 		if (main_pid == 0)
 		{
 			if (i != 0)
 				dup2(fd_in, STDIN_FILENO);
-			if ((i + 1) != 2)
+			if ((i + 1) != ac / 3)
 				dup2(pipefd[1], STDOUT_FILENO);
 			close(pipefd[0]);
 			close(pipefd[1]);
+			close(fd_in);
 			push_dynarray(darr, &nul, 1, 0);
 			ft_find_bin(argv[i * 3], ft_getenvval("PATH", darr->list, darr->nb_cells), &argv[i * 3], darr->list);
 		}
-		else
-		{
-			dup2(pipefd[0], fd_in);
-			//close(pipefd[0]);
-			//close(pipefd[1]);
-		}
+		dup2(pipefd[0], fd_in);
+		//close(pipefd[0]);
+		//close(pipefd[1]);
 		i++;
 	}
 	i = 0;
 	int status = 0;
-	while (i < 2)
+	while (i < ac / 3)
 	{
 		waitpid(list[i], &status, 0);
 		i++;
