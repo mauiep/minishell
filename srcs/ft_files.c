22b0cdacfb3deafde7808ddb44673ff4cede5179
Niki NIKOLOV <ngenadie@e1r12p13.clusters.42paris.fr>
2022-10-11 21:12:26 +0200

    j'ai regler les redirections

--- srcs/ft_files.c
+++ srcs/ft_files.c
@@ -10,12 +10,10 @@ int	ft_open_create(char *filename, bool apnd_or_not)
 	pathname = ft_strjoin(getcwd(buffer, 10000), "/");
 	pathname2 = ft_strjoin(pathname, filename);
 	free(pathname);
-	dprintf(2, "PATHNAME2 = %s\n", pathname2);
-	fd = open(pathname2, O_APPEND * apnd_or_not);
+	fd = open(pathname2, O_RDWR | O_APPEND * apnd_or_not);
 	if (fd == -1)
 	{
-		fd = open(pathname2, O_CREAT & O_APPEND * apnd_or_not);
-		dprintf(2, "CREATING\n");
+		fd = open(pathname2, O_RDWR | O_CREAT | O_APPEND * apnd_or_not, S_IRWXU);
 		if (fd == -1)
 			return (-1);
 	}
